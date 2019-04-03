3e8ae1246611c23cffd9d13e908160ab2d5d995d
linliangzhao <283477489@.com>
2019-04-03 14:23:59 -0400

    data analysis of metric1234

--- data analysis/matlab/coefficient.m
+++ data analysis/matlab/coefficient.m
@@ -2,19 +2,23 @@ clc
 clear
 close all
 %statementcoverage
-statementcoverage=[
-];
+statementcoverage=[];
 %branchcoverage
 branchcoverage=[];
 %mutation score
 mutationscore=[];
-%cyct
-cyct=[];
+%cxty
+cxty=[];
 
-plot(branchcoverage,mutationscore,'*r')
+
+plot(branchcoverage,cxty,'*r')
+xlabel("Branch Coverage")
+ylabel("Cyclomatic complexity")
 %xlim([0 10])
 %ylim([0 10])
+%statementcoverage，branchcoverage，cxty，mutationscore。
+%Statement Coverage，Branch Coverage，Mutation Score，Cyclomatic complexity
 %皮尔森相关系数
-r_pearson=corr(branchcoverage,mutationscore,'type','pearson');
+r_pearson=corr(branchcoverage,cxty,'type','pearson');
 %斯皮尔曼相关系数
-r_spearman=corr(branchcoverage,mutationscore,'type','spearman');
+r_spearman=corr(branchcoverage,cxty,'type','spearman');
