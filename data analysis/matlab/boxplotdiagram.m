68b8a65cacf331607816b3d290d41469648af4e5
linliangzhao <283477489@.com>
2019-04-16 23:34:17 -0400

    document of data analysis,conclusion and some figures for paper

--- data analysis/matlab/boxplotdiagram.m
+++ data analysis/matlab/boxplotdiagram.m
@@ -2,7 +2,7 @@ clc
 clear
 close all
 x1=[1,2,3,4,5,6,9;9,6,7,8,9,5,4;9,6,7,8,9,5,4;9,6,7,8,9,5,4;9,6,7,8,9,5,4];
-x=x1';
+x=[];
 y1=[2,3,4,5,6,7];
 %boxplot(x,{'Germany', 'USA', 'Japan', 'Italy', 'France'})
 boxplot(x);
